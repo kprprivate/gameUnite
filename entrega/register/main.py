ed57edd164729c8c38ad07992c8ef1c5ed654fc6
Jamaral2802 <joaovitor.vamaral@gmail.com>
2025-05-07 00:20:05 -0300

    nova

--- entrega/register/main.py
+++ entrega/register/main.py
@@ -5,17 +5,27 @@ app.secret_key = "chave_secreta_segura"
 
 users = {}
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         nome = request.form.get('nome')
         email = request.form.get('email')
         senha = request.form.get('senha')
+        confirmar_senha = request.form.get('confirmar_senha')
 
-        if senha == "aaaa":
+        if email in users:
+            flash("Email já registrado!", "error")
+
+        elif senha != confirmar_senha:
+            flash("As senhas não coincidem!", "error")
+
+        elif senha == "aaaa":
             flash("Registro simulado com sucesso!", "success")
+
         else:
-            flash("Senha inválida para simulado!", "error")
+            users[email] = {'nome': nome, 'senha': senha}
+            flash("Cadastro realizado com sucesso!", "success")
 
         return redirect(url_for('register'))
 
