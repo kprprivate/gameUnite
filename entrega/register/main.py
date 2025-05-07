4d0e8512f73a5dbadabebbc7a01f5ad2ee726f69
Jamaral2802 <joaovitor.vamaral@gmail.com>
2025-05-07 00:14:43 -0300

    Registro

--- entrega/register/main.py
+++ entrega/register/main.py
@@ -1,14 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = "chave_secreta_segura"
 
-@app.route("/register", methods=["GET"])
+users = {}
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    username = request.args.get('username')
+    if request.method == "POST":
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
 
-    print(username)
+        if senha == "aaaa":
+            flash("Registro simulado com sucesso!", "success")
+        else:
+            flash("Senha inválida para simulado!", "error")
 
-    return "Hello World!"
+        return redirect(url_for('register'))
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
